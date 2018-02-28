31501d9689ed470632c98686d0997565fbca418e
Fiona Whittington <fionaw@bu.edu>
2018-02-28 18:08:29 -0500

    Adding javascript files

---
+++ config.js
@@ -0,0 +1,13 @@
+//config.js
+/** TWITTER APP CONFIGURATION
+ * consumer_key
+ * consumer_secret
+ * access_token
+ * access_token_secret
+ */
+module.exports = {
+  consumer_key: 'kquhhyUgdRCFkGYIUEiUe8piP',  
+  consumer_secret: 'I3LPLblIO0DoRLBK4IV6n764iDyVbE7H96Szd6W78pp10BnSrI',
+  access_token: '968979884344307713-GwaHGXOx72GYjddgOMEgQpct5PJxE9E',  
+  access_token_secret: 'mI81TFKbleWlr6uVZRrpsSCyYbzpWVVJNNiiPLW6KkcE6'
+}
